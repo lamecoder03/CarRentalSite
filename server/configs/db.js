138cc397fe2bfb853a4886da7fa1418d243478d3
Archit Adnaik <architadnaik96@gmail.com>
2026-02-14 20:36:15 +0530

    Update db.js

--- server/configs/db.js
+++ server/configs/db.js
@@ -1,13 +1,20 @@
 import mongoose from "mongoose";
 
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
 const connectDB = async () => {
-  try {
-    console.log("Mongo URI exists:", !!process.env.MONGODB_URI);
-    await mongoose.connect(process.env.MONGODB_URI);
-    console.log("Database Connected");
-  } catch (error) {
-    console.log("DB Error:", error);
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(process.env.MONGODB_URI).then((mongoose) => mongoose);
   }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
 };
 
 export default connectDB;
